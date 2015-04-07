d97668d054cb52481825284b509b45b619c9d0d8
Moritz Schmidt <fusselwurm@gmail.com>
2015-04-07 22:08:27 +0200

    wip: restructure models
    * have one class for men and vehicles (models.Unit)
    * for Side stuff, use strings from arma, dont invent own stuff
    
    ... also, I will save models as redis string (json)
        instead of a hash. this falls neatly into the
        'why did I not do this the first time round?' category

---
+++ lib/arma.ts
@@ -0,0 +1,115 @@
+/// <reference path="./../typings/tsd.d.ts" />
+
+import bunyan = require('bunyan')
+import models = require('./models')
+
+var logger = bunyan.createLogger({name: 'arma'});
+
+function intMapper(value: any) {
+    return parseInt(value, 10);
+}
+
+function setId(model: models.Unit, id: any) {
+    id = parseInt(id, 10);
+    if (!id) {
+        throw new TypeError('invalid id ' + typeof id);
+    }
+    model.id = id;
+}
+
+function setName(model: models.Unit, name: any) {
+    if (!name) {
+        return;
+    }
+    name = ('' + name).substr(0, 32);
+
+    model.name = name;
+}
+
+function setPosition(model: models.Unit, position: any) {
+    if (!position) {
+        return;
+    }
+    if (!Array.isArray(position) || position.length > 3) {
+        throw new TypeError('invalid position ' + typeof position);
+    }
+    position = position.map(intMapper);
+
+    model.position = position;
+}
+
+function setDirection(model: models.Unit, direction: any) {
+    if (!direction && typeof direction !== 'number') {
+        return;
+    }
+    model.direction = parseInt(direction, 10);
+}
+
+function setSide(model: models.Unit, side: any) {
+    if (!side) {
+        return;
+    }
+    if (models.Side.values.indexOf(side) !== -1) {
+        model.side = side;
+        return;
+    }
+    logger.warn('ignoring unknown side ' + side);
+}
+
+function setHealth(model: models.Unit, health: any) {
+    if (!health) {
+        return;
+    }
+    if (models.Health.values.indexOf(health) !== -1) {
+        model.health = health;
+    } else {
+        logger.warn('ignoring unknown health' + health);
+    }
+}
+
+function setIcon(model: models.Unit, icon: any) {
+    if (!icon) {
+        return;
+    }
+
+    if (typeof icon !== 'string') {
+        throw new TypeError('icon must be string: ' + typeof icon);
+    }
+
+    model.icon = icon;
+}
+
+
+function setContainer(unit: models.Unit, container: any) {
+    if (!container && typeof container !== 'number') {
+        return;
+    }
+    unit.container = container;
+}
+
+export function setContent(unit: models.Unit, content: any) {
+    if (!content) {
+        return;
+    }
+    if (!Array.isArray(content)) {
+        throw new TypeError('content must be array');
+    }
+    unit.content = content.map(intMapper)
+}
+
+
+export function toUnit(datum: Array<any>): models.Unit {
+
+    var unit = new models.Unit();
+    setId(unit, datum[0]);
+    setPosition(unit, datum[1]);
+    setDirection(unit, datum[2]);
+    setSide(unit, datum[3]);
+    setHealth(unit, datum[4]);
+    setIcon(unit, datum[5]);
+    setName(unit, datum[6]);
+    setContainer(unit, datum[7]);
+    setContent(unit, datum[8]);
+
+    return unit;
+}
---
+++ lib/models.ts
@@ -0,0 +1,61 @@
+
+export class Side {
+    static WEST = 'WEST';
+    static EAST = 'EAST';
+    static GUER = 'GUER';
+    static CIV = 'CIV';
+    static AMBIENT_LIFE = 'AMBIENT LIFE';
+    static ENEMY = 'ENEMY';
+    static LOGIC = 'LOGIC';
+    static UNKNOWN = 'UNKNOWN';
+
+    static values = [Side.WEST, Side.EAST, Side.GUER, Side.CIV, Side.AMBIENT_LIFE, Side.ENEMY, Side.LOGIC, Side.UNKNOWN];
+}
+
+export class Health {
+    static ALIVE = 'alive';
+    static UNCONSCIOUS = 'unconscious';
+    static DEAD = 'dead';
+
+    static values = [Health.ALIVE, Health.UNCONSCIOUS, Health.DEAD];
+}
+
+class Model {
+    static fromJSON(json): Unit {
+        var
+            obj = JSON.parse(json),
+            result = new Unit();
+
+        Object.keys(result).forEach(function (key) {
+            if (obj[key] !== undefined) {
+                result[key] = obj[key];
+            }
+        });
+
+        return result;
+    }
+
+    toJSON(): string {
+        var result: any = {};
+
+        Object.keys(this).forEach(function (key) {
+            if (this[key] !== undefined) {
+                result[key] = this[key];
+            }
+        });
+
+        return JSON.stringify(result);
+    }
+}
+
+export class Unit extends Model {
+    id: number;
+    position: Array<number>;
+    direction: number;
+    side: Side;
+    health: Health;
+    icon: string;
+    name: string;
+    content: Array<number>;
+    container: number;
+}
